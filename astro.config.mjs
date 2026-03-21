525282a03addc3013933bf6c089dcd02fde30cb8
SdTech1610 <sdtech1610@gmail.com>
2026-03-21 19:27:02 +0530

    update base url of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.sdtechworld.in',
-  base: '/sdtech_blog',
+  base: '/',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx(),
