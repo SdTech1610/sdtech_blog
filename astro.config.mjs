800cd77532db0b02dd66b45579c0a4aa630df21f
SdTech1610 <sdtech1610@gmail.com>
2026-03-21 18:14:41 +0530

    add sdtechworld in the website and add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devpulse.blog',
+  site: 'https://blog.sdtechworld.in',
   integrations: [
     tailwind({ applyBaseStyles: false }),
     mdx(),
